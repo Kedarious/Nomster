6a143dce48c8236913d72b8990645d1ab108aa8b
kedarious <darious_petway@yahoo.com>
2019-06-08 07:15:40 +0000

    whatever the message

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,43 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]	
+  def index
+    @places = Place.all
+  end
 
-	def index
-		@places = Place.all
-	end
+  def new
+    @place = Place.new
+  end
 
-	def new
-		@place = Place.new
-	end	
 
-		def create
-			Place.create(place_params)
-			redirect_to root_path
-	end	
+  def create
+    current_user.places.create(place_params)
+  redirect_to root_path
+  end
 
-	private
+  def show
+  	@place = Place.find(params[:id])
+  end
 
-	def place_params
-		params.require(:place).permit(:name, :description, :address)
+  def edit
+  	@place = Place.find(params[:id])
+  end	
+
+  def update
+  	@place = Place.find(params[:id])
+  	@place.update_attributes(place_params)
+  	redirect_to root_path
+  end
+
+  def destroy
+  	@place = Place.find(params[:id])
+  	@place.destroy
+  	redirect_to root_path
+  end	
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
