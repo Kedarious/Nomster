34e0f9fafcad8bd7f423de9039526959c45d1700
kedarious <darious_petway@yahoo.com>
2019-06-25 00:33:39 +0000

    whatever the message

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ApplicationRecord
-	belongs_to: user
-	belongs_to: place
-	mount_uploader :photo, PictureUploader
+	mount_uploader :picture, PictureUploader
+	belongs_to	:user
+	belongs_to	:place
+	
 end	
